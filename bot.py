983d4366eff26dce0e854cbb743272f3b551f47e
Com-munist <78481868+Com-munist@users.noreply.github.com>
2024-10-19 16:52:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,20 @@ intents.message_content = True  # Enable privileged message content intent (requ
 
 # Discord bot setup
 TOKEN = os.getenv('DISCORD_TOKEN')  # Replace with your actual bot token
+CHANNEL_ID = 1296422821269995572
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-
+     # Get the channel by ID
+    channel = bot.get_channel(CHANNEL_ID)
+    
+    if channel:
+        await channel.send('Hello, this is my first message in this channel!')
+    else:
+        print(f'Channel with ID {CHANNEL_ID} not found.')
 # Run the bot
 bot.run(TOKEN)
 
